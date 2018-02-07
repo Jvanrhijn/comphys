5401d997f13f91e7ec445944cab06b4caf361ec8
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-02-07 22:19:52 +0100

    Implement diff. eq. solvers, almost finish part 2

--- comphys.py
+++ comphys.py
@@ -27,8 +27,41 @@ def excitons1b():
     ax.grid()
     plt.show()
 
+
 def excitons1c():
-    pass
+    def potential(x):
+        return 0.5*x**2
+    # Eigenvalue guess
+    eigenvalue_guess = 1.5
+    # Set up equidistant grid
+    grid_points = 500
+    grid_displacement = 0.1
+    grid_end = 4
+    grid = np.linspace(0, grid_end, grid_points) + grid_displacement
+    # Get classical turning point index
+    turning_point = shooting.outer_turning_point_newton(potential, eigenvalue_guess, grid, len(grid)//2)
+    # Set up initial values of forward and backward solutions
+    solution_forward_first = grid[0]
+    solution_forward_second = grid[1]
+    solution_backward_last = np.exp(-np.sqrt(eigenvalue_guess)*grid[-1])
+    solution_backward_second_last = np.exp(-np.sqrt(eigenvalue_guess)*grid[-2])
+    # Solve the differential equation
+    solution_forward = shooting.solve_equation_forward(solution_forward_first, solution_forward_second,
+                                                       grid, potential, eigenvalue_guess)
+    solution_backward = shooting.solve_equation_backward(solution_backward_last, solution_backward_second_last,
+                                                         grid, potential, eigenvalue_guess)
+    # Match the solutions at the turning point
+    solution_forward /= solution_forward[turning_point]
+    solution_backward /= solution_backward[turning_point]
+    # Plot both solutions
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(grid[0:turning_point], solution_forward[0:turning_point])
+    ax.plot(grid[turning_point:], solution_backward[turning_point:])
+    ax.set_xlabel(r"$\rho$")
+    ax.set_ylabel(r"$\zeta(\rho)$")
+    ax.grid()
+    plt.show()
 
 
 if __name__ == "__main__":
--- lib/shooting.py
+++ lib/shooting.py
@@ -7,17 +7,17 @@ import matplotlib.pyplot as plt
 import unittest
 
 
-def outer_turning_point_newton(effective_potential, energy, grid, guess) -> int:
+def outer_turning_point_newton(potential, energy, grid, guess) -> int:
     """Find index of outer turning point in grid, by Newton's method
 
-    :param effective_potential: effective potential energy function
+    :param potential: effective potential energy function
     :param energy: energy eigenvalue of particle
     :param grid: grid for use in numerical computation
     :param guess: initial guess for index, needed for Newton
     :return: index of turning point in grid
     """
     dx = min(np.diff(grid))*0.001
-    root = find_root_newton(lambda x: effective_potential(x) - energy, 10, grid[guess], dx=dx)
+    root = find_root_newton(lambda x: potential(x) - energy, 10, grid[guess], dx=dx)
     index = abs(grid - root).argmin()
     return index
 
@@ -70,16 +70,67 @@ def solution_backward_next(grid_point, previous, pprevious, potential, energy, s
     return next_value
 
 
-class ShootingTest(unittest.TestCase):
-    """ Test cases for the functions in this module
+def solve_equation_forward(solution_first, solution_second, grid, potential, energy) -> np.ndarray:
+    """Solves the differential equation by forward propagation
 
+    :param solution_first: the solution at the first grid point
+    :param solution_second: the solution at the second grid point
+    :param grid: the grid to solve the equation on
+    :param potential: the potential function to solve the equation for
+    :param energy: the energy eigenvalue to solve the equation for
+    :return: solution (np.ndarray) obtained by forward propagation
     """
-    def find_root_newton(self):
+    solution = np.zeros(len(grid))
+    solution[0], solution[1] = solution_first, solution_second
+    turning_point_index = outer_turning_point_newton(potential, energy, grid, len(grid)//2)
+    for n in range(2, turning_point_index + 2):
+        step_size = grid[n] - grid[n-1]
+        solution[n] = solution_forward_next(grid[n-1], solution[n-1], solution[n-2], potential, energy, step_size)
+    return solution
+
+
+def solve_equation_backward(solution_last, solution_second_last, grid, potential, energy) -> np.ndarray:
+    """Solves the differential equation by backward propagation
+
+    :param solution_last: value of solution at end point
+    :param solution_second_last: value of solution at point prior to end point
+    :param grid: grid to solve the equation on
+    :param potential: the potential function to solve the equation for
+    :param energy: the energy eigenvalue to solve the equation for
+    :return: solution (np.ndarray) obtained by forward propagation
+    """
+    solution = np.zeros(len(grid))
+    solution[-1], solution[-2] = solution_last, solution_second_last
+    turning_point_index = outer_turning_point_newton(potential, energy, grid, len(grid)//2)
+    for n in range(len(grid) - 3, turning_point_index - 2, -1):
+        step_size = abs(grid[n] - grid[n-1])
+        solution[n] = solution_backward_next(grid[n], solution[n+1], solution[n+2], potential, energy, step_size)
+    return solution
+
+
+def glue_arrays_together(first_half, second_half, at_index, priority=1) -> np.ndarray:
+    assert(len(first_half) == len(second_half))
+    if priority != 1 and priority != 2:
+        raise(ValueError("Priority kwarg must be either '1' or '2'"))
+    length = len(first_half)
+    if priority == 1:
+        for n in range(at_index + 1, length):
+            first_half[n] = second_half[n]
+        return first_half
+    elif priority == 2:
+        for n in range(length - 1, -1, -1):
+            second_half[n] = first_half[n]
+        return second_half
+
+
+class ShootingTest(unittest.TestCase):
+    """ Test cases for the functions in this module"""
+    def test_find_root_newton(self):
         """Test newton root finding algorithm"""
         self.assertAlmostEqual(find_root_newton(lambda x: (x**2 - 1), 5, 0.5), 1, 3)
         self.assertAlmostEqual(find_root_newton(lambda x: (x**2 - 1), 5, -.5), -1, 3)
         with self.assertRaises(TypeError):
-            find_root_newton(lambda x: x, 1, 0.1)
+            find_root_newton(lambda x: x, "foo", 0.1)
 
     def test_outer_turning_point(self):
         self.assertEqual(outer_turning_point_newton(lambda x: x**2, 1, np.linspace(0, 10, 1000), 50), 100)
@@ -102,6 +153,15 @@ class ShootingTest(unittest.TestCase):
         with self.assertRaises(AssertionError):
             solution_backward_next(1, 1, 1, lambda x: x, 1, -0.01)
         self.assertEqual(solution_backward_next(0, 0, 0, lambda x: x, 0, 0.01), 0)
+
+    def test_glue_together(self):
+        first = np.array([1, 2, 3, 4, 0, 0, 0, 0, 0, 0])
+        second = np.array([0, 0, 3, 4, 5, 6, 7, 8, 9, 10])
+        np.testing.assert_equal(glue_arrays_together(first, second, 3), np.arange(1, 11))
+        with self.assertRaises(AssertionError):
+            glue_arrays_together(np.array([0, 1, 2]), np.array([0, 1]), 1)
+        with self.assertRaises(ValueError):
+            glue_arrays_together(np.array([0, 1, 2]), np.array([-1, 1, 0]), 1, priority=3)
 
 
 if __name__ == '__main__':
