c60870becc6753716aa56128c3c872e3949bbc5b
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-02-07 09:32:11 +0100

    Add test skeleton

---
+++ shooting/shooting.py
@@ -0,0 +1,18 @@
+""" This module contains functions to solve the 'Shooting' project
+
+Tests for each function are written at the bottom of the file
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import unittest
+
+
+class ShootingTest(unittest.TestCase):
+    """ Test cases for the functions in this module
+
+    """
+    def test_outer_turning_point(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
