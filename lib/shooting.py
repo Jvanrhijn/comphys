f4ec40f4720be39798f2f0802d4223a1d4deeff7
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-02-13 18:28:06 +0100

    Fix style issues

--- lib/shooting.py
+++ lib/shooting.py
@@ -16,6 +16,7 @@ def outer_turning_point(potential, energy, grid) -> int:
     :return: index of turning point in grid
     """
     index = abs(potential(grid) - energy).argmin()
+
     return index
 
 
@@ -31,8 +32,8 @@ def solution_next(previous, p_previous, potential, energy, step_size, *grid_poin
     :return: value of solution at next grid point
     """
     assert(step_size > 0)
-    next_value = 2 * previous - p_previous + step_size ** 2 * (potential(grid_points[0]) - energy) * previous
-    return next_value
+    return 2 * previous - p_previous + step_size ** 2 * (potential(grid_points[0]) - energy) * previous
+
 
 
 def solution_next_numerov(previous, p_previous, potential, energy, step_size, *grid_points) -> float:
@@ -47,10 +48,12 @@ def solution_next_numerov(previous, p_previous, potential, energy, step_size, *g
     :return: value of solution at next (backward) grid point
     """
     assert(step_size > 0)
+
     next_value = (5*step_size**2/6*(potential(grid_points[0]) - energy) + 2) * previous - (
             1 - step_size**2/12
             * (potential(grid_points[1]) - energy)) * p_previous
     next_value /= 1 - step_size**2/12*(potential(grid_points[2]) - energy)
+
     return next_value
 
 
@@ -69,14 +72,17 @@ def solve_equation_forward(solution_first, solution_second, grid, potential, ene
     """
     solution = np.zeros(len(grid))
     solution[0], solution[1] = solution_first, solution_second
+
     if numerov:
         propagate = solution_next_numerov
     else:
         propagate = solution_next
+
     for n in range(2, turning_point_index + 2):
         step_size = grid[n] - grid[n-1]
         solution[n] = propagate(solution[n-1], solution[n-2], potential, energy, step_size,
                                 grid[n-1], grid[n-2], grid[n])
+
     return solution
 
 
@@ -95,15 +101,19 @@ def solve_equation_backward(solution_last, solution_second_last, grid, potential
     """
     solution = np.zeros(len(grid))
     solution[-1], solution[-2] = solution_last, solution_second_last
+
     # Choose algorithm
+
     if numerov:
         propagate = solution_next_numerov
     else:
         propagate = solution_next
+
     for n in range(len(grid) - 3, turning_point_index - 2, -1):
         step_size = grid[n] - grid[n-1]
         solution[n] = propagate(solution[n+1], solution[n+2], potential, energy, step_size,
                                 grid[n+1], grid[n+2], grid[n])
+
     return solution
 
 
@@ -119,7 +129,9 @@ def glue_arrays_together(first_half, second_half, at_index, overwrite=1) -> np.n
     assert(len(first_half) == len(second_half))
     if overwrite != 1 and overwrite != 2:
         raise(ValueError("Overwrite kwarg must be either '1' or '2'"))
+
     length = len(first_half)
+
     if overwrite == 1:
         for n in range(at_index + 1, length):
             first_half[n] = second_half[n]
@@ -131,7 +143,7 @@ def glue_arrays_together(first_half, second_half, at_index, overwrite=1) -> np.n
 
 
 def normalize_solution(grid, solution) -> np.ndarray:
-    """Normalize wave function
+    """Normalize wave function using trapezoidal integration
 
     :param grid: grid to integrate on
     :param solution: non-normalized solution of differential equation
@@ -139,6 +151,7 @@ def normalize_solution(grid, solution) -> np.ndarray:
     """
     integral = np.trapz(solution**2, grid)
     solution /= np.sqrt(integral)
+
     return solution
 
 
@@ -161,12 +174,13 @@ def solve_equation(solution_first, solution_second, solution_last, solution_seco
                                               turning_point_index, numerov=numerov)
     solution_backward = solve_equation_backward(solution_last, solution_second_last, grid, potential, energy,
                                                 turning_point_index, numerov=numerov)
-    # Match forward & backward solutions
+
+    # Match forward and backward solutions and normalize
     solution_forward /= solution_forward[turning_point_index]
     solution_backward /= solution_backward[turning_point_index]
     solution = glue_arrays_together(solution_forward, solution_backward, turning_point_index)
-    # Normalize
     solution = normalize_solution(grid, solution)
+
     return solution
 
 
@@ -200,23 +214,28 @@ def shooting_iteration_bisection(grid, solution_first, solution_second, solution
     mid_point = 0.5*(right_bound + left_bound)
     turning_point_left = outer_turning_point(potential, left_bound, grid)
     turning_point_mid = outer_turning_point(potential, mid_point, grid)
+
+    # Calculate forward & backward solutions for the lower bound & the interval midpoint
     solution_forward_left = solve_equation_forward(solution_first, solution_second, grid, potential, left_bound,
                                                    turning_point_left, numerov=numerov)
     solution_backward_left = solve_equation_backward(solution_last, solution_second_last, grid, potential, left_bound,
                                                      turning_point_left, numerov=numerov)
-    solution_forward_left /= solution_forward_left[turning_point_left]
-    solution_backward_left /= solution_backward_left[turning_point_left]
-    left_derivative_continuity = continuity_measure_function(solution_forward_left, solution_backward_left,
-                                                             turning_point_left)
     solution_forward_mid = solve_equation_forward(solution_first, solution_second, grid, potential, mid_point,
                                                   turning_point_mid, numerov=numerov)
     solution_backward_mid = solve_equation_backward(solution_first, solution_second, grid, potential, mid_point,
                                                     turning_point_mid, numerov=numerov)
+
+    solution_forward_left /= solution_forward_left[turning_point_left]
+    solution_backward_left /= solution_backward_left[turning_point_left]
     solution_backward_mid /= solution_backward_mid[turning_point_mid]
     solution_forward_mid /= solution_forward_mid[turning_point_mid]
+
+    left_derivative_continuity = continuity_measure_function(solution_forward_left, solution_backward_left,
+                                                             turning_point_left)
     mid_derivative_continuity = continuity_measure_function(solution_forward_mid, solution_backward_mid,
                                                             turning_point_mid)
-    # Get new interval
+
+    # Narrow down the interval containing the root accoring to the bisection algoritm
     if np.sign(left_derivative_continuity) == np.sign(mid_derivative_continuity):
         return mid_point, right_bound, mid_derivative_continuity
     else:
@@ -240,22 +259,28 @@ def shooting_iteration_improved(solution_first, solution_second, solution_last, 
     """
     step_size = min(np.diff(grid))
     turning_point = outer_turning_point(potential, eigenvalue_guess, grid)
+
     solution_left = solve_equation_forward(solution_first, solution_second,
                                            grid, potential, eigenvalue_guess,
                                            turning_point, numerov=numerov)
     solution_right = solve_equation_backward(solution_last, solution_second_last,
                                              grid, potential, eigenvalue_guess,
                                              turning_point, numerov=numerov)
+
     solution_left /= solution_left[turning_point]
     solution_right /= solution_right[turning_point]
+
     # Use trapezoidal integration to calculate factor 'A'
     factor = (np.trapz(solution_left[:turning_point]**2, grid[:turning_point])
               + np.trapz(solution_right[turning_point:]**2, grid[turning_point:]))**-1
     derivative_continuity = continuity_measure_function(solution_left, solution_right, turning_point)
+
     # Get next eigenvalue guess
     eigenvalue_guess -= factor*derivative_continuity/(2*step_size)
+
     # Check for convergence
     converged = (abs(derivative_continuity*factor) < tolerance)
+
     return eigenvalue_guess, derivative_continuity, converged
 
 
@@ -269,36 +294,42 @@ def shooting_method(grid, solution_first, solution_second, solution_last, soluti
     :param solution_second: solution at second grid point
     :param solution_last: solution at last grid point
     :param solution_second_last: solution at second last grid point
-    :param turning_point: turning point
     :param tolerance: convergence tolerance
     :param max_iterations: maximum iterations to run
     :param potential: potential function to solve equation for
     :param algorithm_inputs: inputs required for algorithm, bracket for bisection, initial guess for improved algorithm
     :param algorithm: algorithm to use for finding roots, either 'bisection' or 'improved'
-    :param numerov: whether to use numerov algorithm in DE solver
+    :param numerov: whether to use Numerov algorithm in DE solver
     :return: eigenvalue obtained using the shooting method
     """
     assert(algorithm == 'bisection' or algorithm == 'improved')
     iterations = 0
     continuity_iterations, eigenvalues = [], []
     converged = False
+
     if algorithm == 'bisection':
         left_bound = algorithm_inputs[0]
         right_bound = algorithm_inputs[1]
+
         while not converged and iterations < max_iterations:
             old_root_guess = 0.5*(right_bound + left_bound)
-            left_bound, right_bound, mid_continuity = shooting_iteration_bisection(grid, solution_first,
-                                                                                   solution_second, solution_last,
-                                                                                   solution_second_last, left_bound,
-                                                                                   right_bound, potential,
-                                                                                   numerov=numerov)
-            continuity_iterations.append(mid_continuity)
+            left_bound, right_bound, mid_continuity = shooting_iteration_bisection(
+                                                                                grid, solution_first,
+                                                                                solution_second, solution_last,
+                                                                                solution_second_last, left_bound,
+                                                                                right_bound, potential,
+                                                                                numerov=numerov
+                                                                                )
             new_root_guess = 0.5*(right_bound + left_bound)
+
+            continuity_iterations.append(mid_continuity)
             eigenvalues.append(new_root_guess)
+
             iterations += 1
             converged = (abs(new_root_guess - old_root_guess) < tolerance)
     elif algorithm == 'improved':
         eigenvalue_guess = algorithm_inputs[0]
+
         while not converged and iterations < max_iterations:
             eigenvalue_guess, derivative_continuity, converged = shooting_iteration_improved(
                                                                                   solution_first, solution_second,
@@ -306,8 +337,11 @@ def shooting_method(grid, solution_first, solution_second, solution_last, soluti
                                                                                   grid, potential, eigenvalue_guess,
                                                                                   tolerance, numerov=numerov
                                                                                   )
-            continuity_iterations.append(derivative_continuity), eigenvalues.append(eigenvalue_guess)
+            continuity_iterations.append(derivative_continuity)
+            eigenvalues.append(eigenvalue_guess)
+
             iterations += 1
+
     return eigenvalues, continuity_iterations
 
 
@@ -322,11 +356,13 @@ def generate_latex_table(left_column, right_column, header_left, header_right, r
     :return: string containing LaTeX formatted table
     """
     table = "{0} & {1}\\\\\\hline\n".format(header_left, header_right)
+
     for left, right in zip(left_column, right_column):
         if rounding is not None:
             left = round(left, rounding)
             right = round(right, rounding)
         table += str(left) + " & " + str(right) + "\\\\ \n"
+
     return table
 
 
