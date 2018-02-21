a6c9b3105ced9e7fb2dc751cc788b6a01cff6c29
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-02-21 13:39:33 +0100

    Start work on SpinConfiguration object

--- lib/monte_carlo.py
+++ lib/monte_carlo.py
@@ -26,3 +26,63 @@ class IsingModel(MonteCarlo):
         self._magnetic_field = magnetic_field
         self._lattice_side = lattice_side
 
+
+class SpinConfiguration(object):
+    """Object representing spins on a lattice"""
+    def __init__(self, lattice):
+        self._lattice = lattice
+
+    @classmethod
+    def all_up(cls, rows, columns):
+        """Initialize configuration with all spins up"""
+        return cls(np.ones((rows, columns)))
+
+    @classmethod
+    def all_down(cls, rows, columns):
+        """Initialize configuration with all spins down"""
+        return cls(np.ones((rows, columns))*-1)
+
+    @classmethod
+    def init_random(cls, rows, columns):
+        """Initialize a random spin configuration in the desired shape"""
+        return None
+
+    def __setitem__(self, row, column, value):
+        """Set a spin to a value"""
+        pass
+
+    def __getitem__(self, row, column):
+        pass
+
+    def magnetization(self):
+        """Returns magnetization of the lattice"""
+        return np.sum(self._lattice)
+
+    def flip_spin(self, row, column):
+        pass
+
+
+class SpinConfigTest(unittest.TestCase):
+    """Tests for SpinConfiguration class"""
+    def test_initialize(self):
+        some_lattice = np.array([[1, -1], [-1, -1]])
+        config = SpinConfiguration(some_lattice)
+        config_up = SpinConfiguration.all_up(5, 5)
+        config_down = SpinConfiguration.all_down(5, 5)
+        np.testing.assert_array_equal(config._lattice, some_lattice)
+        np.testing.assert_array_equal(config_up._lattice, np.ones((5, 5)))
+        np.testing.assert_array_equal(config_down._lattice, np.ones((5, 5))*-1)
+        random_lattice = SpinConfiguration.init_random(10, 10)
+        SpinConfiguration(random_lattice._lattice)  # Check if init_random returns a valid lattice
+        with self.assertRaises(ValueError):
+            SpinConfiguration(np.array([[1, 2], [-1, 1]]))
+
+    def test_magnetization(self):
+        some_lattice = np.array([[1, -1], [-1, -1]])
+        config = SpinConfiguration(some_lattice)
+        magnetization = np.sum(some_lattice)
+        self.assertEqual(config.magnetization(), magnetization)
+
+
+if __name__ == '__main__':
+    unittest.main()
