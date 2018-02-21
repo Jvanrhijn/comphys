30be26ce9f30272828a36241d95da4681494ddd1
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-02-21 12:34:21 +0100

    Start work on IsingModel class

--- lib/monte_carlo.py
+++ lib/monte_carlo.py
@@ -4,4 +4,25 @@ import lib.util as util
 import unittest
 
 
+class MonteCarlo(object):
+    """Base Monte Carlo simulator, defines virtual interface"""
+    def __init__(self, num_runs):
+        self._num_runs = num_runs
+
+    def init_state(self):
+        pass
+
+    def plot_state(self, *args, **kwargs):
+        pass
+
+    def variable_error(self):
+        pass
+
+
+class IsingModel(MonteCarlo):
+    """Ising model solver using Monte Carlo method"""
+    def __init__(self, num_runs, magnetic_field, lattice_side):
+        super().__init__(num_runs)
+        self._magnetic_field = magnetic_field
+        self._lattice_side = lattice_side
 
