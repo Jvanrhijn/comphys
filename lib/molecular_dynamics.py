5a5cd34c3b27f3746c69ae8b4b79bfd2260a2aa6
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-03-27 16:12:43 +0200

    Start implementing verlet integrator

--- lib/molecular_dynamics.py
+++ lib/molecular_dynamics.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 
 
 class State:
+    """Encodes a many-particle state.
+    This class encodes the state of a collection of N classical particles, in d spatial dimensions (default 3)
+    """
     def __init__(self, num_particles, dim=3):
         self._num_particles = num_particles
         self._dim = dim
@@ -19,13 +22,46 @@ class State:
     def velocities(self) -> np.ndarray:
         return self._velocities
 
+    @positions.setter
+    def positions(self, new_pos) -> None:
+        self._positions = new_pos
+
+    @velocities.setter
+    def velocities(self, new_vel) -> None:
+        self._velocities = new_vel
+
     def forces(self, forces_function) -> np.ndarray:
+        """Calculate the forces on the current state given a force function"""
         return forces_function(self._positions)
 
     def init_random(self, position_range: tuple, velocity_range: tuple):
+        """
+        Initialize the State with random positions and velocities in a given range
+        :param position_range: range of positions (2-tuple)
+        :param velocity_range: range of velocities (2-tuple)
+        :return: Reference to the current state
+        """
         self._positions = np.random.random(size=(self._dim, self._num_particles))\
             * (position_range[1] - position_range[0]) + position_range[0]
         self._velocities = np.random.random(size=(self._dim, self._num_particles))\
             * (velocity_range[1] - velocity_range[0]) + velocity_range[0]
         return self
+
+
+class VerletIntegrator:
+    """Iterator that yields the state of """
+    def __init__(self, init_state, force_function, time_step):
+        self._state = init_state
+        self._half_velocity = 0
+        self._forces = force_function
+        self._time_step = time_step
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self._half_velocity = self._state.velocities + 0.5*self._forces(self._state.positions)*self._time_step
+        self._state.positions += self._half_velocity*self._time_step
+        self._state.velocities = self._half_velocity + 0.5*self._forces(self._state.positions)*self._time_step
+        return self._state
 
--- tests/tests_molecular_dynamics.py
+++ tests/tests_molecular_dynamics.py
@@ -18,3 +18,23 @@ class TestState(unittest.TestCase):
         test_state = md.State(3, 10).init_random((-1, 1), (-1, 1))
         forces = test_state.forces(lambda pos: force_sho(1, pos))
         nptest.assert_array_almost_equal(forces, test_state.positions*-1)
+
+    def test_integrator_step(self):
+        no_force = lambda positions: 0
+        constant_force = lambda positions: 1
+        sho_foce = lambda positions: -positions
+        init_state = md.State(1, dim=1)
+        dt = 1
+        integrator = md.VerletIntegrator(init_state, no_force, dt)
+        next_step = next(integrator)
+        nptest.assert_array_equal(next_step.positions, md.State(1, dim=1).positions)  # For no force, state remains
+        nptest.assert_array_equal(next_step.velocities, md.State(1, dim=1).velocities)
+
+        integrator = md.VerletIntegrator(init_state, constant_force, dt)
+        next_step = next(integrator)
+        after_constant = md.State(1, dim=1)
+        after_constant.positions = np.array([[0.5]])
+        after_constant.velocities = np.array([[1]])
+        nptest.assert_array_almost_equal(next_step.positions, after_constant.positions)
+        nptest.assert_array_almost_equal(next_step.velocities, after_constant.velocities)
+
